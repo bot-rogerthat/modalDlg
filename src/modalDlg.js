20b8abe46f679c0953703ce6168c2427ec8e9610
Sergej Yakushenok <sergej_yakushenok@epam.com>
2016-06-16 22:17:28 +0300

    first impl modalDlg

---
+++ src/modalDlg.js
@@ -0,0 +1,53 @@
+class ModalDlg {
+    constructor(title, message, ...buttons) {
+        this.title = title;
+        this.message = message;
+        this.buttons = buttons;
+        this.selector = $('<div>');
+        this.overlay = $('<div>');
+    }
+
+    show() {
+        this.createDialog();
+        this.createOverlay();
+        this.createButtons();
+
+        $('.overlay').fadeIn(200, function () {
+            $('.dialog').css('display', 'block').animate({opacity: 1, top: '50%'}, 100);
+        });
+        $('.overlay').on('click', this.hide);
+    }
+
+    hide() {
+        $('.dialog').animate({opacity: 0, top: '45%'}, 100, function () {
+            $('.dialog').css('display', 'none');
+            $('.overlay').fadeOut(100);
+            $(this).children().remove();
+        });
+    }
+
+    createDialog() {
+        this.selector.addClass('dialog');
+        this.selector.appendTo('body');
+        $('<h1>').html(this.title).appendTo(this.selector);
+        $('<p>').text(this.message).appendTo(this.selector);
+    }
+
+    createOverlay() {
+        this.overlay.addClass('overlay');
+        this.overlay.appendTo('body');
+    }
+
+    createButtons() {
+        var dlg = this.selector;
+        var self = this;
+        $.each(this.buttons, function (i, val) {
+            $('<button/>').text(val.title).on('click', function () {
+                val.func();
+                self.hide();
+            }).addClass('modalDlgButton').appendTo(dlg);
+        });
+    }
+}
+
+
